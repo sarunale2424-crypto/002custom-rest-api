0c0f3764c7f282873d5c5d4d3f4eac1c08deebc9
=sanu <sarun.ale2424@gmail.com>
2025-11-16 23:23:43 +0900

    Creating itemModels

--- models/itemModels.go
+++ models/itemModels.go
@@ -1,1 +1,19 @@
 package models
+
+import "time"
+
+type Item struct {
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func NewItem(id uint, name string, price float64, created_at time.Time) Item {
+	return Item{
+		ID:        id,
+		Name:      name,
+		Price:     price,
+		CreatedAt: created_at,
+	}
+}
